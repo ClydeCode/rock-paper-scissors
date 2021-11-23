7432a252c2fd038305fe2108a9d7c07b3cc8bfdd
Clyde <noneclyde@gmail.com>
2021-11-23 16:46:28 +0200

    Add new function playRound()

--- javascript.js
+++ javascript.js
@@ -8,9 +8,32 @@ function computerPlay() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
 
   if (randomNumber == 1)
-    return "Rock"
+    return "rock"
   if (randomNumber == 2)
-    return "Papper"
+    return "papper"
   if (randomNumber == 3)
-    return "Scissors"
+    return "scissors"
+}
+
+function playRound(playerSelection, computerSelection) {
+  playeroption = playerSelection.toLowerCase()
+
+  if (playeroption == "rock" && computerSelection == "rock")
+    return "Tie!"
+  if (playeroption == "rock" && computerSelection == "papper")
+    return "You lose! Papper beats Rock!"
+  if (playeroption == "rock" && computerSelection == "scissors")
+    return "You won! Rock beats Scissors"
+  if (playeroption == "papper" && computerSelection == "rock")
+    return "You won! Papper beats Rock"
+  if (playeroption == "papper" && computerSelection == "papper")
+    return "Tie!"
+  if (playeroption == "papper" && computerSelection == "scissors")
+    return "You lose! Scissors beats Papper"
+  if (playeroption == "scissors" && computerSelection == "rock")
+    return "You lose! Rock beats Scissors"
+  if (playeroption == "scissors" && computerSelection == "papper")
+    return "You won! Scissors beats papper"
+  if (playeroption == "scissors" && computerSelection == "scissors")
+    return "Tie!"
 }
