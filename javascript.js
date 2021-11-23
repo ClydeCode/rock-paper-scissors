ea20428590f32fb2700ef186328e07595d3414bf
Clyde <noneclyde@gmail.com>
2021-11-23 17:22:20 +0200

    Add new function input() and input error handling

--- javascript.js
+++ javascript.js
@@ -10,31 +10,40 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  playeroption = playerSelection.toLowerCase()
-
-  if (playeroption == "rock" && computerSelection == "rock")
+  if (playerSelection == "rock" && computerSelection == "rock")
     return "Tie!"
-  if (playeroption == "rock" && computerSelection == "papper")
+  if (playerSelection == "rock" && computerSelection == "papper")
     return "You lose! Papper beats Rock!"
-  if (playeroption == "rock" && computerSelection == "scissors")
+  if (playerSelection == "rock" && computerSelection == "scissors")
     return "You won! Rock beats Scissors"
-  if (playeroption == "papper" && computerSelection == "rock")
+  if (playerSelection == "papper" && computerSelection == "rock")
     return "You won! Papper beats Rock"
-  if (playeroption == "papper" && computerSelection == "papper")
+  if (playerSelection == "papper" && computerSelection == "papper")
     return "Tie!"
-  if (playeroption == "papper" && computerSelection == "scissors")
+  if (playerSelection == "papper" && computerSelection == "scissors")
     return "You lose! Scissors beats Papper"
-  if (playeroption == "scissors" && computerSelection == "rock")
+  if (playerSelection == "scissors" && computerSelection == "rock")
     return "You lose! Rock beats Scissors"
-  if (playeroption == "scissors" && computerSelection == "papper")
+  if (playerSelection == "scissors" && computerSelection == "papper")
     return "You won! Scissors beats papper"
-  if (playeroption == "scissors" && computerSelection == "scissors")
+  if (playerSelection == "scissors" && computerSelection == "scissors")
     return "Tie!"
+}
+
+function input() {
+  while(true) {
+  text = prompt("Choose: Rock, Papper or Scissors").toLowerCase()
+
+  if (text == "rock" || text == "papper" || text == "scissors")
+    return text
+  else
+    console.log("Wrong Input!")
+  }
 }
 
 function game() {
   for (let counts = 0; counts < 5; counts++) {
-    console.log(playRound(prompt("Choose: Rock, Papper or Scissors"), computerPlay()))
+    console.log(playRound(input(), computerPlay()))
   }
 }
 
