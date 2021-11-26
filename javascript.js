604615d456cbd94e11a1aec166de1497aee1872c
Clyde <noneclyde@gmail.com>
2021-11-26 15:06:15 +0200

    Add score checking, if statements for buttons and Query Selectors for divs and buttons

--- javascript.js
+++ javascript.js
@@ -9,36 +9,58 @@ function computerPlay() {
     return "scissors";
 }
 
+function resetScore() {
+  playerScore = 0;
+  computerScore = 0;
+}
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "rock")
-    return "Tie! Result: " + playerScore + ":" + computerScore;
+    return "Tie! Player: " + playerScore + ":" + computerScore + " Computer";
   if (playerSelection == "rock" && computerSelection == "papper")
-    return "You lose! Papper beats Rock! Result: " + playerScore + ":" + ++computerScore;
+    return "You lose! Papper beats Rock! Player: " + playerScore + ":" + ++computerScore + " Computer";
   if (playerSelection == "rock" && computerSelection == "scissors")
-    return "You won! Rock beats Scissors! Result: " + ++playerScore + ":" + computerScore;
+    return "You won! Rock beats Scissors! Player: " + ++playerScore + ":" + computerScore + " Computer";
   if (playerSelection == "papper" && computerSelection == "rock")
-    return "You won! Papper beats Rock! Result: " + ++playerScore + ":" + computerScore;
+    return "You won! Papper beats Rock! Player: " + ++playerScore + ":" + computerScore + " Computer";
   if (playerSelection == "papper" && computerSelection == "papper")
-    return "Tie! Result: " + playerScore + ":" + computerScore;
+    return "Tie! Player: " + playerScore + ":" + computerScore + " Computer";
   if (playerSelection == "papper" && computerSelection == "scissors")
-    return "You lose! Scissors beats Papper! Result: " + playerScore + ":" + ++computerScore;
+    return "You lose! Scissors beats Papper! Player: " + playerScore + ":" + ++computerScore + " Computer";
   if (playerSelection == "scissors" && computerSelection == "rock")
-    return "You lose! Rock beats Scissors! Result: " + playerScore + ":" + ++computerScore;
+    return "You lose! Rock beats Scissors! Player: " + playerScore + ":" + ++computerScore + " Computer";
   if (playerSelection == "scissors" && computerSelection == "papper")
-    return "You won! Scissors beats papper! Result: " + ++playerScore + ":" + computerScore;
+    return "You won! Scissors beats papper! Player: " + ++playerScore + ":" + computerScore + " Computer";
   if (playerSelection == "scissors" && computerSelection == "scissors")
-    return "Tie! Result: " + playerScore + ":" + computerScore;
+    return "Tie! Player: " + playerScore + ":" + computerScore + " Computer";
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
 const buttons = document.querySelectorAll('button');
+const content = document.querySelector('.res');
+const finalContent = document.querySelector('.finalRes');
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    if (button.id == 1) console.log(playRound('rock', computerPlay()));
-    if (button.id == 2) console.log(playRound('papper', computerPlay()));
-    if (button.id == 3) console.log(playRound('scissors', computerPlay()));
+
+    if (finalContent.textContent != null) finalContent.textContent = '';
+
+    if (button.id == 1) content.textContent = (playRound('rock', computerPlay()));
+    
+    if (button.id == 2) content.textContent = (playRound('papper', computerPlay()));
+    
+    if (button.id == 3) content.textContent = (playRound('scissors', computerPlay()));
+    
+    if (playerScore == 5) {
+      finalContent.textContent = 'You won this match!';
+      resetScore();
+    }
+
+    if (computerScore == 5) {
+      finalContent.textContent = 'You lose this match!';
+      resetScore();
+    }
   });
 });
